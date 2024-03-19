703b0dc833ad36754d7c45e86e466dadae9a5d3e
bentobarpp <bentobarpp@gmail.com>
2024-03-19 17:59:31 -0300

    atualizando

--- eleitor.py
+++
@@ -1,9 +0,0 @@
-idade = int(input("Informe sua Idade: "))
-if idade <= 16:
-    print("Voce nao e leitor")
-elif idade >= 18 and idade <= 65:
-    print("Voce e eleitor obrigatorio! ")
-elif idade == 17 and idade >=66:
-    print("Voce e eleitor facultativo! ")
-
-
--- idade de servico.py
+++
@@ -1,6 +0,0 @@
-idade = int(input("Informe sua idade: "))
-if idade >= 66:
-    print("Voce pode se aposentar!")
-elif idade <= 65:
-    print("Voce nao pode se aposentar! ")
-
